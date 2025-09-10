fd50c7b3e705db6d427ae3b28cebebbb7b64abd6
Rónai Márton János <ronai.marton.janos@diak.szbi-pg.hu>
2025-09-10 11:22:56 +0200

    feladatok mappa

---
+++ 02_adattipusok.py
@@ -0,0 +1,13 @@
+name = "Marci"
+age = 17
+hight = 1.80
+is_student = True
+# print(f"name: {name} age: {age} hight: {hight} is student: {is_student}")
+print(f""" name: {name}
+      age: {age}
+      hight: {hight}
+      is student: {is_student}""")
+print(type(name))
+print(type(age))
+print(type(hight))
+print(type(is_student))
